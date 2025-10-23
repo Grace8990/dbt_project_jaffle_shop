85a86cd4e5707f911eb89b05356828b6fca69e4d
Grace Chen <xc576@cornell.edu>
2025-10-23 04:33:31 +0000

    Add  testing and stripe tables

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,7 +1,17 @@
-select
+with
+
+source as (
+    select * from {{ source('jaffle_shop', 'customers') }}
+),
+renamed as (
+
+    select
         id as customer_id,
         first_name,
         last_name
+    
+    from source
+)
 
-    from {{ source('jaffle_shop', 'customers') }}
+select * from renamed
 
